7a6af3e49222291b6f57c2a5c0bb058d72999a80
demosofa <62540039+demosofa@users.noreply.github.com>
2023-08-16 15:54:05 +0700

    Hotfix (#28)
    * fix: category create and update still receive parentCategory even though its id was null
    
    * fix: add missing auth checking, try catch to delete file upload if fail and add checking if existed
    
    * fix: add missing auth checking, try catch to delete file upload if fail and add checking if existed
    
    * chore: delete unused code
    
    * fix: allow fe to load image without being blocked

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-	app.use(helmet());
+	app.use(helmet({ crossOriginResourcePolicy: false }));
 	app.enableCors();
 	app.useGlobalInterceptors(new DeleteInterceptor());
 	app.useGlobalPipes(
